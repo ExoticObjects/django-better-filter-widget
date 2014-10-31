5f6b913bd95d0bc66fe1219e2ac97f5d4d8b8aba
Exotic Objects <git@extc.co>
2014-10-31 15:19:42 -0400

    v0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-better-filter-widget',
-    version='0.1',
+    version='0.2',
     author='Exotic Objects LLC',
     author_email='git@extc.co',
     license='MIT',
